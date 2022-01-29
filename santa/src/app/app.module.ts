efa3f580be2a10072f1fb69bbc52f8e2492995fd
Cole Thompson <wolvelscotch@yahoo.com>
2022-01-28 20:58:25 -0600

    create register component

--- santa/src/app/app.module.ts
+++ santa/src/app/app.module.ts
@@ -4,14 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './account/register/register.component';
+import { LoginComponent } from './account/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
